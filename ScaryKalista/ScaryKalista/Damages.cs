c52b0888dbd977d44fb70799e2d6ed375d4343c3
Monstertje <monstertje_3@mail.com>
2016-05-23 00:13:21 +0200

    Fix rend damage

--- ScaryKalista/ScaryKalista/Damages.cs
+++ ScaryKalista/ScaryKalista/Damages.cs
@@ -8,7 +8,6 @@ namespace ScaryKalista
         public static readonly Damage.DamageSourceBoundle QDamage = new Damage.DamageSourceBoundle();
 
         private static readonly float[] RawRendDamage = { 20, 30, 40, 50, 60 };
-        private static readonly float[] RawRendDamageMultiplier = { 0.6f, 0.6f, 0.6f, 0.6f, 0.6f };
         private static readonly float[] RawRendDamagePerSpear = { 10, 14, 19, 25, 32 };
         private static readonly float[] RawRendDamagePerSpearMultiplier = { 0.2f, 0.225f, 0.25f, 0.275f, 0.3f };
 
@@ -36,7 +35,7 @@ namespace ScaryKalista
         public static float GetRendDamage(Obj_AI_Base target)
         {
             return Player.Instance.CalculateDamageOnUnit(target, DamageType.Physical, GetRawRendDamage(target)) *
-                   (Player.Instance.HasBuff("summonerexhaust") ? 0.6f : 1);
+                   (Player.Instance.HasBuff("summonerexhaust") ? 0.6f : 0.98f);
         }
 
         public static float GetRawRendDamage(Obj_AI_Base target)
@@ -45,8 +44,8 @@ namespace ScaryKalista
             if (stacks > -1)
             {
                 var index = Spells.E.Level - 1;
-                return RawRendDamage[index] + stacks * RawRendDamagePerSpear[index] +
-                       Player.Instance.TotalAttackDamage * (RawRendDamageMultiplier[index] + stacks * RawRendDamagePerSpearMultiplier[index]);
+                return (RawRendDamage[index] + (0.6f * Player.Instance.TotalAttackDamage)) + 
+                    ((stacks) * (RawRendDamagePerSpear[index] + (RawRendDamagePerSpearMultiplier[index] * Player.Instance.TotalAttackDamage)));
             }
 
             return 0;
